bc222ce006d378a1b13ee9695c9c4867a23cfea5
unknown <leonelvallejos3@gmail.com>
2024-08-25 21:36:23 -0300

    Aplicacion

--- Actividad/Funciones.py
+++ Actividad/Funciones.py
@@ -1,8 +1,8 @@
-def suma(a,b):
+def suma(a, b):
     return a + b
 
-def resta(a,b):
+def resta(a, b):
     return a - b
 
-def multiplicacion(a,b):
+def multiplicacion(a, b):
     return a * b
--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 from Actividad.Funciones import suma, resta, multiplicacion
 
 def prueba_suma():
-    assert suma(1,2) == 3
+    assert suma(1, 2) == 3
 
 def prueba_resta():
-    assert resta(2,2) == 0
+    assert resta(2, 2) == 0
 
 def prueba_multiplicacion():
-    assert multiplicacion(1,1) == 1
+    assert multiplicacion(1, 1) == 1
